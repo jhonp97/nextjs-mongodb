bfa515aa73239fbefdfec641b5bb8624ab80f761
jhonp97 <perezjhon2010@alumnos.cei.es>
2025-06-13 23:18:00 +0200

    act

--- src/server/usuarios.js
+++ src/server/usuarios.js
@@ -2,17 +2,26 @@
 
 // traer todos los postS
 
+// export const getJsonUsuarios = async () => {
+//   if (!process.env.NEXT_PUBLIC_API) return [];
+
+//   try {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_API}/usuarios`);
+//     if (!res.ok) throw new Error('Respuesta no OK');
+//     const data = await res.json();
+//     return data.data;
+//   } catch (error) {
+//     console.error('Error al obtener usuarios:', error);
+//     return [];
+//   }
+// };
+
+// lib/usuarios.js
+import clientPromise from "@/lib/db2";
+
 export const getJsonUsuarios = async () => {
-  if (!process.env.NEXT_PUBLIC_API) return [];
-
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/usuarios`);
-    if (!res.ok) throw new Error('Respuesta no OK');
-    const data = await res.json();
-    return data.data;
-  } catch (error) {
-    console.error('Error al obtener usuarios:', error);
-    return [];
-  }
+  const client = await clientPromise;
+  const db = client.db(); // Usa el nombre que configuraste en DATABASE si es necesario
+  const usuarios = await db.collection('usuarios').find().toArray();
+  return usuarios;
 };
-
